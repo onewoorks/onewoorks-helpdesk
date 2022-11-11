74e5ca55daf2ac67356696037696fe6389551578
irwan ibrahim <irwanbibrahim@onewoorks.com>
2022-11-11 11:53:58 +0800

    theme adjustment

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -21,5 +21,22 @@ if (($lang = Internationalization::getCurrentLanguage()) && $lang != 'en_US') { 
         print $api->client(false);
     ?>);
 </script>
+
+<script>
+        (function(d,t) {
+          var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+          g.src="https://app.chaskiq.io/embed.js"
+          s.parentNode.insertBefore(g,s);
+          g.onload=function(){
+            new window.ChaskiqMessengerEncrypted({
+              domain: 'https://app.chaskiq.io',
+              ws:  'wss://app.chaskiq.io/cable',
+              app_id: "gpUbFdnqqihCHPKLjtCKRzjJ",
+              data: "YOUR_ENCRYPTED_JWE_DATA",
+              lang: "USER_LANG_OR_DEFAULTS_TO_BROWSER_LANG" 
+            })
+          }
+        })(document,"script");
+      </script>
 </body>
 </html>
